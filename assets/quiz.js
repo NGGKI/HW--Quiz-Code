ba8a565e5f2790d997da1a31e182893b6fe73562
NGGKI <kien.dv2012@gmail.com>
2022-02-26 12:24:42 -0800

    adding comment on javascipt

--- assets/quiz.js
+++ assets/quiz.js
@@ -18,191 +18,191 @@ var viewScore = document.getElementById("viewscore");
 
 // create some variables
 var textInner = "";
-
 var saved = localStorage.getItem("saved");
 
-// create our questions
+// create questions
 let questions = [
-  {
-    question:
-      "How many chances told by Doctor Strange after his vision, that the world can be saved from Thanos invasion?  ",
-    choiceA: "10",
-    choiceB: "5",
-    choiceC: "1",
-    correct: "C",
-  },
-  {
-    question: "What does CSS stand for?",
-    choiceA: "Chris Evans",
-    choiceB: "Daniel Craig",
-    choiceC: "Jim Carrey",
-    correct: "A",
-  },
-  {
-    question: "What does JS stand for?",
-    choiceA: "Pepper Potts",
-    choiceB: "Tony Stark",
-    choiceC: "Morgan Stark",
-    correct: "C",
-  },
+    {
+        question:
+            "How many chances told by Doctor Strange after his vision, that the world can be saved from Thanos invasion?  ",
+        choiceA: "10",
+        choiceB: "5",
+        choiceC: "1",
+        correct: "C",
+    },
+    {
+        question: "What does CSS stand for?",
+        choiceA: "Chris Evans",
+        choiceB: "Daniel Craig",
+        choiceC: "Jim Carrey",
+        correct: "A",
+    },
+    {
+        question: "What does JS stand for?",
+        choiceA: "Pepper Potts",
+        choiceB: "Tony Stark",
+        choiceC: "Morgan Stark",
+        correct: "C",
+    },
 ];
+
 
 const lastQuestion = questions.length - 1;
 let runningQuestion = 0;
 
 // render a question
 function renderQuestion() {
-  let q = questions[runningQuestion];
+    let q = questions[runningQuestion];
 
-  question.innerHTML = "<p>" + q.question + "</p>";
-  choiceA.innerHTML = q.choiceA;
-  choiceB.innerHTML = q.choiceB;
-  choiceC.innerHTML = q.choiceC;
+    question.innerHTML = "<p>" + q.question + "</p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
 }
+
 
 start.addEventListener("click", startQuiz);
 
 // start quiz
 function startQuiz() {
-  start.style.display = "none";
-  viewScore.style.display = "none";
-  document.getElementById("para").style.display = "none";
-  renderQuestion();
-  quiz.style.display = "block";
-  renderProgress();
-  startTimer();
+    start.style.display = "none";
+    viewScore.style.display = "none";
+    document.getElementById("para").style.display = "none";
+    renderQuestion();
+    quiz.style.display = "block";
+    renderProgress();
+    startTimer();
 }
 
 // render progress
 function renderProgress() {
-  for (let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
-    progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
-  }
+    for (let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
+        progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
+    }
 }
 
 //timer
 var sec = 50;
 function startTimer() {
-  window.countTimer = setInterval(function () {
-    sec--;
-    document.getElementById("timer").innerHTML = sec;
-    if (sec <= 0) {
-      clearInterval(window.countTimer);
-      alert("you lost!");
-    }
-  }, 1000);
+    window.countTimer = setInterval(function () {
+        sec--;
+        document.getElementById("timer").innerHTML = sec;
+        if (sec <= 0) {
+            clearInterval(window.countTimer);
+            alert("you lost!");
+        }
+    }, 1000);
 }
 // checkAnwer
-
 function checkAnswer(answer) {
-  if (answer == questions[runningQuestion].correct) {
-    answerIsCorrect();
-  } else {
-    answerIsWrong();
-  }
-  count = 0;
-  if (runningQuestion < lastQuestion) {
-    runningQuestion++;
-    renderQuestion();
-  } else {
-    // end the quiz and show the score
-    clearInterval(window.countTimer);
-    scoreRender();
-  }
+    if (answer == questions[runningQuestion].correct) {
+        answerIsCorrect();
+    } else {
+        answerIsWrong();
+    }
+    count = 0;
+    if (runningQuestion < lastQuestion) {
+        runningQuestion++;
+        renderQuestion();
+    } else {
+        // end the quiz and show the score
+        clearInterval(window.countTimer);
+        scoreRender();
+    }
 }
 
 // answer is correct
 function answerIsCorrect() {
-  document.getElementById("result").innerText = "Right!";
+    document.getElementById("result").innerText = "Right!";
 }
 
 // answer is Wrong
 function answerIsWrong() {
-  document.getElementById("result").innerText = "Wrongggg!";
-  sec -= 10;
+    document.getElementById("result").innerText = "Wrongggg!";
+    sec -= 10;
 }
 
 // score render
 function scoreRender() {
-  scoreDiv.style.display = "block";
-  quiz.style.display = "none";
-  const textTimeRemaining = `Your score is ${sec}`;
-  myScore.textContent = textTimeRemaining + " points";
+    scoreDiv.style.display = "block";
+    quiz.style.display = "none";
+    const textTimeRemaining = `Your score is ${sec}`;
+    myScore.textContent = textTimeRemaining + " points";
 }
 
+// create an Array for score list
 function getArrayScore() {
-  var localArrayScore = [];//block scope
+    var localArrayScore = [];//block scope
 
-  if (localStorage.getItem("arrayScore")) {
-    localArrayScore = localStorage.getItem("arrayScore");
-    localArrayScore = JSON.parse(localArrayScore);
-  }
+    if (localStorage.getItem("arrayScore")) {
+        localArrayScore = localStorage.getItem("arrayScore");
+        localArrayScore = JSON.parse(localArrayScore);
+    }
 
-  console.log(localArrayScore)
-  return localArrayScore;
+   /*  console.log(localArrayScore) */
+    return localArrayScore;
 }
 
+// create function to render Scorelist in text
 function renderScoreList(tagHTML, arrayExisted) {
-  var textInner = "";
-  for (const element of arrayExisted) {
-    textInner += `<li> ${element.username}: ${element.timer}`;
-  }
-  // arrayScore.forEach(item => {
-  //     textInner += `<li> ${item.username}: ${item.timer} points </li>`
-  // });
+    var textInner = "";
+    for (const element of arrayExisted) {
+        textInner += `<li> ${element.username}: ${element.timer}`;
+    }
 
-  return (tagHTML.innerHTML = textInner);
+    return (tagHTML.innerHTML = textInner);
 }
 
 // create function to handle the form
 function handleFormSubmit(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  var userName = $('input[name="name-input"]').val();
+    var userName = $('input[name="name-input"]').val();
 
-  // page load
+    var newScore = { username: userName, timer: sec };
 
-  var newScore = { username: userName, timer: sec };
+    const arrayScore = getArrayScore();
 
-  const arrayScore = getArrayScore();
+    arrayScore.push(newScore); // push any new info to local storage
+    var stringArray = JSON.stringify(arrayScore);
+    localStorage.setItem("arrayScore", stringArray); // save to local storage
 
-  arrayScore.push(newScore); // push any new info to local storage
-  var stringArray = JSON.stringify(arrayScore);
-  localStorage.setItem("arrayScore", stringArray); // save to local storage
+    if (!userName) {
+        alert("Enter your name, pleassssse!");
+        return;
+    }
 
-  if (!userName) {
-    alert("Enter your name, pleassssse!");
-    return;
-  }
+    renderScoreList(scoreList, arrayScore);
 
-  renderScoreList(scoreList, arrayScore);
+    $('input[name="name-input"]').val("");
 
-  $('input[name="name-input"]').val("");
-
-  scoreForm.style.display = "none";
-  afterGame.style.display = "block";
+    scoreForm.style.display = "none";
+    afterGame.style.display = "block";
 }
 
+// create button function to handle input of player name and score. + save all data in local storage
 saveBtn.addEventListener("click", handleFormSubmit);
 
+// clear local storage for score list
 reset.addEventListener("click", () => {
-  localStorage.removeItem("arrayScore");
-  scoreList.innerHTML = "";
+    localStorage.removeItem("arrayScore");
+    scoreList.innerHTML = "";
 });
 
+// create button go back to main page
 goBack.addEventListener("click", function () {
-  document.location.reload(true);
+    document.location.reload(true);
 });
 
+//create function for view score button
 viewScore.addEventListener("click", () => {
-  viewScore.style.display = "none";
-  document.getElementById("para").style.display = "none";
-  document.getElementById("start").style.display = "none";
-  document.getElementById("scoreform").style.display = "none";
-  document.getElementById("scoreContainer").style.display = "block";
-  document.getElementById("aftergame").style.display = "block";
-  scoreList.style.display = "block";
-
-  const arrayScore = getArrayScore();
-  renderScoreList(scoreList, arrayScore);
+    viewScore.style.display = "none";
+    document.getElementById("para").style.display = "none";
+    document.getElementById("start").style.display = "none";
+    document.getElementById("scoreform").style.display = "none";
+    document.getElementById("scoreContainer").style.display = "block";
+    document.getElementById("aftergame").style.display = "block";
+    scoreList.style.display = "block";
+    const arrayScore = getArrayScore();
+    renderScoreList(scoreList, arrayScore);
 });
